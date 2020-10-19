99902e04d09ad6751af79ff8c3d71328c7fb2cb3
Stepbackru <alex-ruba@bk.ru>
2020-10-19 22:22:32 +0300

    feat: add basic template

--- data/template.js
+++ data/template.js
@@ -1,5 +1,44 @@
 const template = [
   {
-    
+    el: 'div',
+    classNames: 'wrapper main',
+    parent: 'body',
+    child: [
+      {
+        el: 'time',
+        classNames: 'time'
+      },
+      {
+        el: 'div',
+        classNames: 'greating',
+        child: [
+          {
+            el: 'span',
+            classNames: 'greating__time'
+          },
+          {
+            el: 'span',
+            classNames: 'greating__name'
+          }
+        ]
+      },
+      {
+        el: 'div',
+        classNames: 'focus',
+        child: [
+          {
+            el: 'h2',
+            classNames: 'focus__caption',
+            text: 'What is your main focus for today?'
+          },
+          {
+            el: 'input',
+            classNames: 'focus__input',
+            text: 'What is your main focus for today?',
+            dataAttr: [['type', 'text'],['autocomplete', 'off']]
+          }
+        ]
+      },
+    ]
   }
 ]
