745d94980f218c50193f12e38cf4704ee229e9a9
Stepbackru <alex-ruba@bk.ru>
2020-10-23 13:41:50 +0300

    chore: add last row of components

--- components/showTime.js
+++ components/showTime.js
@@ -19,24 +19,3 @@ const addZero = (n) => {
 }
 
 export default showTime;
-
-// // Show Time
-// function showTime() {
-//   let today = new Date(),
-//     hour = today.getHours(),
-//     min = today.getMinutes(),
-//     sec = today.getSeconds();
-
-//   // Set AM or PM
-//   const amPm = hour >= 12 ? 'PM' : 'AM';
-
-//   // 12hr Format
-//   hour = hour % 12 || 12;
-
-//   // Output Time
-//   time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
-//     sec
-//   )} ${showAmPm ? amPm : ''}`;
-
-//   setTimeout(showTime, 1000);
-// }
