52c434e27a7670c2849f441a6460ae29b9845c6e
Stepbackru <alex-ruba@bk.ru>
2020-10-25 11:46:05 +0300

    feat: add focus to input when press keys

--- components/submitForm.js
+++ components/submitForm.js
@@ -2,11 +2,16 @@ import { SUBMIT_KEY, WRAPPER_CLASS, ACCEPT_CLASS } from './constants.js';
 
 const submitForm = (elem, key, modal) => {
   const element = document.querySelector(`.${elem}`);
+  const wrapper = document.querySelector(`.${WRAPPER_CLASS}`);
 
+  document.addEventListener('keypress', (e) => {
+    if (e.key || e.code) {
+      element.focus();
+    }
+  });
+  
   element.addEventListener('keyup', (e) => {
-    
     if (e.key === SUBMIT_KEY || e.code === SUBMIT_KEY) {
-      const wrapper = document.querySelector(`.${WRAPPER_CLASS}`);
       const result = element.value.trim();
 
       if (result.length) {
