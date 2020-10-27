249f30305496aa7f1834286e719b94f9fa88c89f
Stepbackru <alex-ruba@bk.ru>
2020-10-27 19:26:46 +0300

    refactor:  https and remove yandex.location

--- components/getUserCity.js
+++ components/getUserCity.js
@@ -3,18 +3,11 @@ import { CITY_SET, LANG_SET } from './constants.js'
 const getUserCity = async () => {
   const langStore = localStorage.getItem(`${LANG_SET}`);
 
-  try {
-    const url = `http://api.sypexgeo.net/`;
-    const res = await fetch(url);
-    const data = await res.json();
+  const url = `https://api.sypexgeo.net/`;
+  const res = await fetch(url);
+  const data = await res.json();
 
-    localStorage.setItem(`${CITY_SET}`, `${data.city[`name_${langStore}`]}`);
-  } catch {
-    ymaps.ready(function(){
-      const geolocation = ymaps.geolocation;
-      localStorage.setItem(`${CITY_SET}`, `${geolocation.city}`);
-    });
-  }
+  localStorage.setItem(`${CITY_SET}`, `${data.city[`name_${langStore}`]}`);
 }
 
 export default getUserCity;
